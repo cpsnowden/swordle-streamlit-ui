132b7075e4a598ba85f75207bb54ea16f583140c
Chris Snowden <chrissnowden35@gmail.com>
2023-03-20 19:10:32 +1100

    Update UI and README.md

--- Home.py
+++ Home.py
@@ -2,35 +2,42 @@ import requests
 import streamlit as st
 import os
 
-st.set_page_config(page_title="Picture Demo")
+st.set_page_config(page_title="Picture Demo", layout="wide")
 
 DEFAULT_BACKEND_URL = "https://sign-game-server-yckhsn477a-uc.a.run.app"
 BACKEND_URL = os.environ.get("BACKEND_URL", default=DEFAULT_BACKEND_URL)
 BACKEND_API = f"{BACKEND_URL}/letter-prediction/frame"
 
-img_file_buffer = st.camera_input("Take a picture")
+col1, col2 = st.columns(2)
+with col1:
+    st.image(
+        "https://adayinourshoes.com/wp-content/uploads/Sign-Language-Alphabet-PDF.png"
+    )
 
-if img_file_buffer is not None:
-    with st.spinner("Wait for it..."):
-        ### Get bytes from the file buffer
-        img_bytes = img_file_buffer.getvalue()
+with col2:
+    img_file_buffer = st.camera_input("Take a photo of yourself making a sign")
 
-        ### Make request to  API (stream=True to stream response as bytes)
-        print(f"Making request to {BACKEND_API}")
-        res = requests.post(BACKEND_API, files={'img': img_bytes})
+    if img_file_buffer is not None:
+        with st.spinner("Wait for it..."):
+            ### Get bytes from the file buffer
+            img_bytes = img_file_buffer.getvalue()
 
-        json = res.json()
-        if res.status_code == 200:
-            ### Display the prediction returned by the API
-            prediction = json['prediction']
-            st.title(f"Predicted '{prediction.upper()}'")
-        elif res.status_code == 400 and 'detail' in json:
-            ### Display the error returned by the API
-            st.title(json['detail'])
-        else:
-            st.markdown("**Oops**, something went wrong 😓 Please try again.")
-            print(res.status_code, res.content)
+            ### Make request to  API
+            print(f"Making request to {BACKEND_API}")
+            res = requests.post(BACKEND_API, files={'img': img_bytes})
 
-st.image(
-    "https://adayinourshoes.com/wp-content/uploads/Sign-Language-Alphabet-PDF.png"
-)
+            if res.status_code == 200:
+                json = res.json()
+
+                prediction_status = json["predictionStatus"]
+                if prediction_status == "success":
+                    prediction = json['prediction']
+                    st.text(f"Predicted '{prediction.upper()}'")
+                elif prediction_status == "no_hand_detected":
+                    st.text(
+                        "Cound not detect hand, ensure your hand is in the picture"
+                    )
+            else:
+                st.markdown(
+                    "**Oops**, something went wrong 😓 Please try again.")
+                print(res.status_code, res.content)
--- README.md
+++ README.md
@@ -1,13 +1,24 @@
-# sign-game-ui
+# swordle-streamlit-ui
 
-Link to deployed application: https://cpsnowden-sign-game-ui-home-alnzn4.streamlit.app
+This is a simple streamlit ui that was developed to help with testing of the
+Swordle backend, the main product UI is available at [swordle.dev](https://swordle.dev)
+with code available at [swordle-ui](https://github.com/cpsnowden/swordle-ui).
+
+This UI enables someone to take a screenshot which is passed to the SWordle
+backend to predict the ASL letter that is being signed e.g:
+
+![image](images/screenshot.jpeg)
+
+A live version of this UI can be found [here](https://cpsnowden-swordle-streamlit-ui-home-j35s5t.streamlit.app)
+
+## Development
 
 ### Install the application
 
 ```
-pyenv virtualenv sign-game-ui
-pyenv activate sign-game-ui
-pyenv local sign-game-ui
+pyenv virtualenv swordle-streamlit-ui
+pyenv activate swordle-streamlit-ui
+pyenv local swordle-streamlit-ui
 
 make install
 ```
